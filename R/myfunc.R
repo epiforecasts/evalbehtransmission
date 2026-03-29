06614226752739f96d492b72c887420d5e7871a0
lukeburton02 <lukeburton567@gmail.com>
2026-03-29 21:13:47 +0100

    Repo reorganisation, example function and DESCRIPTION

---
+++ R/myfunc.R
@@ -0,0 +1,12 @@
+#' Title
+#'
+#' @param x
+#' @param y
+#'
+#' @returns
+#' @export # Can delete - will the function be internal if making package?
+#'
+#' @examples # For CRAN - can delete
+myfunc <- function(x = 5, y = NA) {
+  return(x^y)
+}
